3a40ad6fbedc6adb6e3a3f7153c78f4895228f78
Manos <manosnikitakis3@gmail.com>
2025-06-17 10:45:07 +0300

    add images

---
+++ src/assets/index.js
@@ -0,0 +1,56 @@
+import yugiohLogo from './yugiohLogo.png'
+import close from './close.svg';
+import menu from './menu.svg';
+import pack1 from './pack1.png';
+import pack2 from './pack2.png';
+import pack3 from './pack3.png';
+import pack4 from './pack4.png';
+import pack5 from './pack5.png';
+import pack6 from './pack6.png';
+import pack10 from './pack10.png';
+import card1 from './card1.png';
+import card2 from './card2.png';
+import card3 from './card3.png';
+import card4 from './card4.png';
+import card5 from './card5.png';
+import card6 from './card6.png';
+import card7 from './card7.png';
+import card8 from './card8.png';
+import card9 from './card9.png';
+import card10 from './card10.png';
+import card11 from './card11.png';
+import card12 from './card12.png';
+import card13 from './card13.png';
+import card14 from './card14.png';
+import card15 from './card15.png';
+import backCard from './backCard.png';
+
+
+export {
+    yugiohLogo,
+    close,
+    menu,
+    pack1,
+    pack2,
+    pack3,
+    pack4,
+    pack5,
+    pack6,
+    pack10,
+    card1,
+    card2,
+    card3,
+    card4,
+    card5,
+    card6,
+    card7,
+    card8,
+    card9,
+    card10,
+    card11,
+    card12,
+    card13,
+    card14,
+    card15,
+    backCard,
+}
