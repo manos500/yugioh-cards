c9f4e5049bda9c7ef8c304c327de0187cc2b347a
Manos <manosnikitakis3@gmail.com>
2025-06-17 10:47:39 +0300

    add cards and packs as objects before fetching from an API

---
+++ src/packs.js
@@ -0,0 +1,88 @@
+import { pack1, pack2, pack3, pack4, pack5, pack6, pack10 } from "./assets/index.js"
+
+export const PACKS = [
+    {
+        id: 1,
+        packName: "Legends of Blue Eyes White Dragon",
+        packImage: pack1,
+        packYear: 2002,
+    },
+    {
+        id: 2,
+        packName: "Metal Raiders",
+        packImage: pack2,
+        packYear: 2002,
+    },
+    {
+        id: 3,
+        packName: "Spell Ruler",
+        packImage: pack3,
+        packYear: 2002,
+    },
+    {
+        id: 4,
+        packName: "Pharaoh's Servant",
+        packImage: pack4,
+        packYear: 2002,
+    },
+    {
+        id: 5,
+        packName: "Labyrinth of Nightmare",
+        packImage: pack5,
+        packYear: 2003,
+    },
+    {
+        id: 6,
+        packName: "Legacy of Darkness",
+        packImage: pack6,
+        packYear: 2003,
+    },
+    {
+        id: 7,
+        packName: "Pharaonic Guardian",
+        packImage: pack3,
+        packYear: 2003,
+    },
+    {
+        id: 8,
+        packName: "Magician's Force",
+        packImage: pack4,
+        packYear: 2003,
+    },
+    {
+        id: 9,
+        packName: "Dark Crisis",
+        packImage: pack4,
+        packYear: 2003,
+    },
+    {
+        id: 10,
+        packName: "Invasion of Chaos",
+        packImage: pack10,
+        packYear: 2004,
+    },
+    {
+        id: 11,
+        packName: "Ancient Sanctuary",
+        packImage: pack2,
+        packYear: 2004,
+    },
+    {
+        id: 12,
+        packName: "Soul of the Duelist",
+        packImage: pack3,
+        packYear: 2004,
+    },
+    {
+        id: 13,
+        packName: "Dark Beginnings 1",
+        packImage: pack4,
+        packYear: 2004,
+    },
+    {
+        id: 14,
+        packName: "Rise of Destiny",
+        packImage: pack4,
+        packYear: 2004,
+    },
+];
